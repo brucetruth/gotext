d417429504f63f1ac8866f65d125fc4cea33c717
Bruce Mubangwa <40401943+brucetruth@users.noreply.github.com>
2021-05-15 22:14:58 +0200

    added methods to be used in near future
    well be using there as we build on

---
+++ utils/jsonf/jsonf.go
@@ -0,0 +1,34 @@
+// Package jsonf provides convenience functions for saving and loading
+// JSON-encoded values to and from the hard drive.
+//
+// Automatically uses gzip streams for gzip files, using the gzipf package.
+package jsonf
+
+import (
+	"encoding/json"
+	"github.com/broosaction/gotext/utils/gzipf"
+)
+
+// Save encodes v as JSON and saves it to the given file.
+func Save(file string, v interface{}) error {
+	f, err := gzipf.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "  ")
+	return enc.Encode(v)
+}
+
+
+
+// Load loads a JSON encoded value from the given file and populates v with it.
+func Load(file string, v interface{}) error {
+	f, err := gzipf.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewDecoder(f).Decode(v)
+}
